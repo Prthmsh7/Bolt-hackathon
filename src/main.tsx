3f82e9687404adfc08d442496d242aed86437b3e
Prathmesh Shukla <115979305+Prthmsh7@users.noreply.github.com>
2025-06-30 16:58:52 +0530

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import './three-global.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
